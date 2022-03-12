565b5bc827a2af7dce85571d436c2585269e6a1b
home-mia <ydjvchslf@gmail.com>
2022-03-12 20:06:08 +0900

    JDK 11 수정, 뷰바인딩, 데이터바인딩 추가

--- app/src/main/java/com/example/memolog/MainActivity.kt
+++ app/src/main/java/com/example/memolog/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.memolog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.memolog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "바꿔주세요오오옹~"
     }
 }
