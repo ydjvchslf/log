be8993b8e54c43c91575009b13c849d1dca8c3dc
poliot-mia <mikyung.yong@poliot.co.kr>
2022-03-17 16:55:12 +0900

    xml 뷰 수정

--- app/src/main/java/com/example/memolog/feature/add/AddViewModel.kt
+++ app/src/main/java/com/example/memolog/feature/add/AddViewModel.kt
@@ -3,4 +3,5 @@ package com.example.memolog.feature.add
 import androidx.lifecycle.ViewModel
 
 class AddViewModel: ViewModel() {
+
 }
---
+++ app/src/main/java/com/example/memolog/feature/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.memolog.feature.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+    
+}
