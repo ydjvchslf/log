2944cb88558acbc2bf2889bbc6c1ed8bc4daf077
poliot-mia <mikyung.yong@poliot.co.kr>
2022-03-25 16:59:32 +0900

    Home 화면 - 백키 앱종료 다이얼로그 추가

--- app/src/main/java/com/example/memolog/feature/home/HomeFragment.kt
+++ app/src/main/java/com/example/memolog/feature/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.memolog.feature.home
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
@@ -7,11 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridLayout
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.memolog.R
 import com.example.memolog.ViewModelFactory
 import com.example.memolog.adapter.RecyclerViewAdapter
 import com.example.memolog.databinding.FragmentHomeBinding
@@ -24,7 +28,7 @@ class HomeFragment : Fragment(){
     private lateinit var binding: FragmentHomeBinding
     lateinit var viewModelFactory: ViewModelFactory
     lateinit var homeViewModel: HomeViewModel
-    //lateinit var recyclerViewAdapter: RecyclerViewAdapter
+    private lateinit var backPressCallback: OnBackPressedCallback
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -66,5 +70,31 @@ class HomeFragment : Fragment(){
     private fun initViewModel(){
         viewModelFactory = ViewModelFactory(MemoRepository())
         homeViewModel = ViewModelProvider(this, viewModelFactory).get(HomeViewModel::class.java)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        backPressCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if(findNavController().currentDestination?.id == R.id.homeFragment) {
+                    Log.d("MemoDebug", "HomeFragment::handleOnBackPressed-()")
+                    AlertDialog.Builder(context)
+                        .setMessage("do you want to exit?")
+                        .setPositiveButton("yes") { _, _ ->
+                            activity?.finish()
+                        }
+                        .setNegativeButton("no") { _, _ ->
+                            // do nothing
+                        }
+                        .show()
+                }
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(this, backPressCallback)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        backPressCallback.remove()
     }
 }
