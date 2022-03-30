91a78bc9720941394c51db9e76c9f84c7c8f5c0e
poliot-mia <mikyung.yong@poliot.co.kr>
2022-03-30 14:35:54 +0900

    search 뷰모델추가

--- app/src/main/java/com/example/memolog/feature/search/SearchFragment.kt
+++ app/src/main/java/com/example/memolog/feature/search/SearchFragment.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.memolog.ViewModelFactory
 import com.example.memolog.databinding.FragmentSearchBinding
+import com.example.memolog.feature.detail.DetailViewModel
+import com.example.memolog.repository.MemoRepository
 
 class SearchFragment: Fragment(){
     private lateinit var binding: FragmentSearchBinding
+    lateinit var viewModelFactory: ViewModelFactory
+    private lateinit var searchViewModel: SearchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,10 +23,22 @@ class SearchFragment: Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
+
+        initViewModel()
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.cancelBtn.setOnClickListener {
+            Toast.makeText(binding.root.context, "취소버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initViewModel() {
+        viewModelFactory = ViewModelFactory(MemoRepository())
+        searchViewModel = ViewModelProvider(this, viewModelFactory).get(SearchViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/example/memolog/feature/search/SearchViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.memolog.feature.search
+
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.memolog.repository.MemoRepository
+import com.example.memolog.repository.entity.Memo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SearchViewModel(private val memoRepository: MemoRepository): ViewModel() {
+
+
+}
