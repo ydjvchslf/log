3c587fbd3e5d62a88a86dbadbc59dde4b601dad6
poliot-mia <mikyung.yong@poliot.co.kr>
2022-04-20 15:42:40 +0900

    홈화면 - 잠금메모일시 Dialog 띄워 비번체크 추가

--- app/src/main/java/com/example/memolog/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/memolog/adapter/RecyclerViewAdapter.kt
@@ -1,10 +1,15 @@
 package com.example.memolog.adapter
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memolog.R
@@ -17,6 +22,7 @@ import com.example.memolog.repository.MemoRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+
 
 class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
 
@@ -49,8 +55,9 @@ class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder
     }
 
     class MyViewHolder(val binding: MemoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val memoRepository = MemoRepository()
+
         fun bind(memo: MemoModel) {
-            val memoRepository = MemoRepository()
             binding.title.text = memo.title
 
             if (memo.isFavorite) { // 좋아요 상태
@@ -61,23 +68,40 @@ class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder
                 binding.likeBtn.visibility = View.VISIBLE
             }
 
+            if (memo.isLocked) binding.lockBtn.visibility = View.VISIBLE else binding.lockBtn.visibility = View.INVISIBLE
+
             // 상세화면 이동
             binding.title.setOnClickListener {
-                //Toast.makeText(binding.root.context, "id : ${memo.id}", Toast.LENGTH_SHORT).show()
-                //it.findNavController().navigate(R.id.action_home_to_detail)
-                when (it.findNavController().currentDestination?.id) {
-                    R.id.homeFragment -> {
-                        it.findNavController()
-                            .navigate(HomeFragmentDirections.actionHomeToDetail(memo.id))
+                CoroutineScope(Dispatchers.IO).launch {
+                    ////////////////////////////////////////////
+                    val memo = memoRepository.selectOne(memo.id)
+                    if(memo.isLocked) { // 잠금 상태인지 확인
+                        // 잠금이면 다이얼로그로 비번 맞는지 확인, true 면 화면 전환O, false 전환X
+                        showCheckPwDialog { password ->
+                            if (password.length == 4 && memo.password == password) {
+                                when (it.findNavController().currentDestination?.id) {
+                                    R.id.homeFragment -> {
+                                        it.findNavController()
+                                            .navigate(HomeFragmentDirections.actionHomeToDetail(memo.id))
+                                    }
+                                    R.id.favoriteFragment -> {
+                                        it.findNavController()
+                                            .navigate(
+                                                FavoriteFragmentDirections.actionFavoriteToDetail(memo.id))
+                                    }
+                                    R.id.searchFragment -> {
+                                        it.findNavController()
+                                            .navigate(
+                                                SearchFragmentDirections.actionSearchToDetail(memo.id))
+                                    }
+                                }
+                            }else{
+                                binding.inputPw.text = null
+                                Toast.makeText(binding.root.context, "비번틀렸슈", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                     }
-                    R.id.favoriteFragment -> {
-                        it.findNavController()
-                            .navigate(FavoriteFragmentDirections.actionFavoriteToDetail(memo.id))
-                    }
-                    R.id.searchFragment -> {
-                        it.findNavController()
-                            .navigate(SearchFragmentDirections.actionSearchToDetail(memo.id))
-                    }
+                    ////////////////////////////////////////////
                 }
             }
 
@@ -96,6 +120,33 @@ class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder
                 //Toast.makeText(binding.root.context, "id : ${memo.id}", Toast.LENGTH_SHORT).show()
                 CoroutineScope(Dispatchers.IO).launch {
                     memoRepository.setFavorite(memo.id, !memo.isFavorite)
+                }
+            }
+        }// fun bind()
+
+        private fun showCheckPwDialog(password: (String) -> Unit){
+            CoroutineScope(Dispatchers.IO).launch {
+
+                Handler(Looper.getMainLooper()).post {
+                    if (binding.inputPw.parent != null) {
+                        (binding.inputPw.parent as ViewGroup).removeView(binding.inputPw)
+                        binding.inputPw.visibility = View.VISIBLE
+                    }
+
+                    AlertDialog.Builder(binding.root.context)
+                        .setMessage("비밀번호를 입력해주세요")
+                        .setView(binding.inputPw)
+                        .setPositiveButton("yes") { _, _ ->
+                            val inputPw = binding.inputPw.text.toString()
+                            Log.d("MemoDebug", "inputPw: $inputPw")
+                            password.invoke(inputPw)
+
+                        }
+                        .setNegativeButton("cancel") { _, _ ->
+                            binding.inputPw.text = null
+                            return@setNegativeButton
+                        }
+                        .show()
                 }
             }
         }
